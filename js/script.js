745613c0281cfddc1ba3ef41f5d48b6175b0acee
Denis <w3prog@gmail.com>
2014-12-03 10:28:33 +0700

    adapted materials under 4 inches

--- js/script.js
+++ js/script.js
@@ -12,8 +12,11 @@ $(document).ready(function() {
       // items : 1, 
       // itemsDesktop : false,
       // itemsDesktopSmall : false,
-      // itemsTablet: false,
-      // itemsMobile : false
+//      itemsDesktop : [1199,4],
+//      itemsDesktopSmall : [980,3],
+//      itemsTablet: [768,2],
+//      itemsTabletSmall: false,
+//      itemsMobile : [479,1]
  
   });
  
